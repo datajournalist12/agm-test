ddad786dfc0b6817ceff29d51e44fe092efdefe4
Alex Heeb <abheeb1s@gmail.com>
2021-02-13 18:39:31 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My AGM project';
+  lat = 37.678418;
+  lng = -89.809007;
+}
