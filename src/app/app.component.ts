30c91268aed5fd299987a10fb5fa8e8b2ce531a3
Alex Heeb <abheeb1s@gmail.com>
2021-02-13 19:52:09 -0600

    fixed issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,13 @@ export class AppComponent {
   title = 'My AGM project';
   lat = 37.678418;
   lng = -89.809007;
+  boolean: boolean = true;
 
   waypoints = [
     {"id":"1", "lat":37.0, "lng":-89.0},
     {"id":"2", "lat":37.1, "lng":-89.1},
-    {"id":"3", "lat":37.4, "lng":-89.4}
+    {"id":"3", "lat":37.8, "lng":-89.4}
   ]
 
-  mapClick(wp) {
-    console.log(wp)
-  }
 
 }
